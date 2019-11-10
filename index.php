24cb0481ae88274298b8314573b2a55776e73813
小白-白 <xb@fczbl.vip>
2019-11-10 12:51:02 +0800

    [Update] Add More Models

--- index.php
+++ index.php
@@ -2,114 +2,36 @@
 /*
 Plugin Name: live2d看板娘(2233)
 Plugin URI: https://github.com/xb2016/poster-girl-l2d-2233
-Description: 2233娘的live2d看板娘插件(WordPress)，支持换人换装！如果觉得本插件还OK的话，请访问下面的插件主页，给我一个star，谢谢！
+Description: 2233 娘的 live2d 看板娘插件(WordPress)，支持换人换装！为了用户体验，不再支持移动端加载。插件需要 JQuery 支持，如果你的主题没有，请编辑插件 index.php。自定义修改请编辑 waifu-tips.js。
 Author: 小白-白
-Version: 1.7
-Author URI: https://www.fczbl.vip
+Version: 1.8
+Author URI: https://moedog.org
 */
 
 //PATH
 define('l2d_URL',plugins_url('', __FILE__));
-//SETTINGS
-add_action('admin_menu','plugin_l2d');
-function plugin_l2d(){
-    add_options_page('live2d','L2D看板娘设置','manage_options','plugin-l2d','plugin_l2d_option_page');
-}
-function plugin_l2d_option_page(){
-    if(!current_user_can('manage_options')) wp_die('抱歉，您没有权限来更改设置');
-    if(isset($_POST['update_options'])){
-        update_option('plugin_l2d_move',(isset($_POST['move'])&&$_POST['move']=='1'));
-        update_option('plugin_l2d_r18',(isset($_POST['r18'])&&$_POST['r18']=='1'));
-        update_option('plugin_l2d_mobile',(isset($_POST['mobile'])&&$_POST['mobile']=='1'));
-        update_option('plugin_l2d_jq',(isset($_POST['jq'])&&$_POST['jq']=='1'));
-        update_option('plugin_l2d_fa',(isset($_POST['fa'])&&$_POST['fa']=='1'));
-        echo '<div id="message" class="updated fade"><p>设置已保存</p></div>';
-    }
-    $move_yes = get_option('plugin_l2d_move')?' checked ':'';
-    $move_no = get_option('plugin_l2d_move')?'':' checked ';
-    $r18_yes = get_option('plugin_l2d_r18')?' checked ':'';
-    $r18_no = get_option('plugin_l2d_r18')?'':' checked ';
-    $mobile_yes = get_option('plugin_l2d_mobile')?' checked ':'';
-    $mobile_no = get_option('plugin_l2d_mobile')?'':' checked ';
-    $jq_yes = get_option('plugin_l2d_jq')?' checked ':'';
-    $jq_no = get_option('plugin_l2d_jq')?'':' checked ';
-    $fa_yes = get_option('plugin_l2d_fa')?' checked ':'';
-    $fa_no = get_option('plugin_l2d_fa')?'':' checked '; ?>
-    <div class="wrap">
-      <h1>L2D看板娘设置</h1>
-      <form action="options-general.php?page=plugin-l2d" method="post">
-		<style>h2{font-size:20px}h3{font-size:15px;font-weight:700}th{font-weight:200}</style>
-        <?php wp_nonce_field('plugin-l2d-options'); ?>
-        <h2>设置</h2>
-          <h3>随机移动</h3>
-            <table>
-              <th>是否允许看板娘自动移动：</th>
-              <td>
-                <label><input type="radio" name="move" <?php echo $move_yes; ?> value="1" /> 是</label> <label><input type="radio" name="move" <?php echo $move_no; ?> value="0" /> 否</label>
-              </td>
-			</table>
-          <h3>R18#嘿嘿嘿</h3>
-            <table>
-              <th>是否允许随机到全裸模型：</th>
-              <td>
-                <label><input type="radio" name="r18" <?php echo $r18_yes; ?> value="1" /> 是</label> <label><input type="radio" name="r18" <?php echo $r18_no; ?> value="0" /> 否</label>
-              </td>
-			</table>
-          <h3>移动端加载(不建议)</h3>
-            <table>
-              <th>移动端是否加载：</th>
-              <td>
-                <label><input type="radio" name="mobile" <?php echo $mobile_yes; ?> value="1" /> 是</label> <label><input type="radio" name="mobile" <?php echo $mobile_no; ?> value="0" /> 否</label>
-              </td>
-            </table><br>
-        <h2>环境</h2>
-          <p>本插件需要 jQuery 库与 Font Awesome 4.7 支持，如果你的主题没有引用上述项目，请选择加载。</p>
-          <p>关于提示语与监听对象的修改，请直接编辑 js/waifu-tips.js。</p>
-          <h3>加载jQuery库</h3>
-            <table>
-              <th>配置是否加载JQ：</th>
-              <td>
-                <label><input type="radio" name="jq" <?php echo $jq_yes; ?> value="1" /> 是</label> <label><input type="radio" name="jq" <?php echo $jq_no; ?> value="0" /> 否</label>
-              </td>
-            </table>
-          <h3>加载Font Awesome 4.7</h3>
-            <table>
-              <th>配置是否加载FA：</th>
-              <td>
-                <label><input type="radio" name="fa" <?php echo $fa_yes; ?> value="1" /> 是</label> <label><input type="radio" name="fa" <?php echo $fa_no; ?> value="0" /> 否</label>
-              </td>
-            </table><br>
-            <p>emmm 最后 <a href="https://github.com/xb2016/poster-girl-l2d-2233" target="_blank">https://github.com/xb2016/poster-girl-l2d-2233</a> 求star！</p>
-            <p class="submit"><input name="update_options" value="保存设置" type="submit"></p>
-      </form>
-    </div><?php        
-}
 //MAIN
-if(!wp_is_mobile()||get_option('plugin_l2d_mobile')) add_action('wp_footer','l2d_main');
+if(!wp_is_mobile()) add_action('wp_footer','l2d_main');
 function l2d_main(){
-    if(wp_is_mobile()) $l2d_w = 130; else $l2d_w = 220;
-    if(wp_is_mobile()) $l2d_h = 150; else $l2d_h = 250;
-    if(!wp_is_mobile()||!get_option('plugin_l2d_mobile')) $mobi = 1;
     wp_enqueue_style('waifu',l2d_URL.'/css/waifu.min.css',array(),'1.7');
-    if(get_option('plugin_l2d_fa')) wp_enqueue_style('fontawe','https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css',array(),'4.7');
-    wp_enqueue_script('waifu',l2d_URL.'/js/waifu-tips.js',array(),'1.7');
-    $d2l2d = array('xb'=>l2d_URL,'move'=>get_option('plugin_l2d_move'),'mobile'=>$mobi,'r18'=>get_option('plugin_l2d_r18'));
-    wp_localize_script('waifu','l2d',$d2l2d); ?>
-    <div class="l2d_xb<?php if(!get_option('plugin_l2d_mobile')) echo ' mh';?>">
+    //引入 JQuery，如果你的主题不自带（大多数 WP 主题都有），请移除下面一行的注释。
+    //wp_enqueue_script('waifu-jq','https://cdn.jsdelivr.net/npm/jquery@2.1.4/dist/jquery.min.js',array(),'2.1.4');
+    wp_enqueue_script('waifu-l2d','https://cdn.jsdelivr.net/gh/xb2016/kratos-pjax@0.3.6/static/js/live2d.js',array(),'l2d');
+    wp_enqueue_script('waifu',l2d_URL.'/js/waifu-tips.js',array(),'1.7'); ?>
+    <div class="l2d_xb" data-api="<?php echo l2d_URL; ?>">
         <div class="waifu">
-        <div class="waifu-tips"></div>
-        <canvas id="live2d" width="<?php echo $l2d_w; ?>" height="<?php echo $l2d_h; ?>" class="live2d"></canvas>
-        <div class="waifu-tool">
-            <span class="fa fa-home"></span>
-            <span class="fa fa-comments"></span>
-            <span class="fa fa-drivers-license-o"></span>
-            <span class="fa fa-street-view"></span>
-            <span class="fa fa-camera"></span>
-            <span class="fa fa-info-circle"></span>
-            <span class="fa fa-close"></span>
+            <div class="waifu-tips"></div>
+            <canvas id="live2d" width="220" height="250" class="live2d"></canvas>
+            <div class="waifu-tool">
+                <span class="home"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg></span>
+                <span class="comments"><svg width="19" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"></path></svg></span>
+                <span class="drivers-license-o"><svg width="18" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M528 32H48C21.5 32 0 53.5 0 80v16h576V80c0-26.5-21.5-48-48-48zM0 432c0 26.5 21.5 48 48 48h480c26.5 0 48-21.5 48-48V128H0v304zm352-232c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zm0 64c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zm0 64c0-4.4 3.6-8 8-8h144c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H360c-4.4 0-8-3.6-8-8v-16zM176 192c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zM67.1 396.2C75.5 370.5 99.6 352 128 352h8.2c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h8.2c28.4 0 52.5 18.5 60.9 44.2 3.2 9.9-5.2 19.8-15.6 19.8H82.7c-10.4 0-18.8-10-15.6-19.8z"></path></svg></span>
+                <span class="street-view"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M367.9 329.76c-4.62 5.3-9.78 10.1-15.9 13.65v22.94c66.52 9.34 112 28.05 112 49.65 0 30.93-93.12 56-208 56S48 446.93 48 416c0-21.6 45.48-40.3 112-49.65v-22.94c-6.12-3.55-11.28-8.35-15.9-13.65C58.87 345.34 0 378.05 0 416c0 53.02 114.62 96 256 96s256-42.98 256-96c0-37.95-58.87-70.66-144.1-86.24zM256 128c35.35 0 64-28.65 64-64S291.35 0 256 0s-64 28.65-64 64 28.65 64 64 64zm-64 192v96c0 17.67 14.33 32 32 32h64c17.67 0 32-14.33 32-32v-96c17.67 0 32-14.33 32-32v-96c0-26.51-21.49-48-48-48h-11.8c-11.07 5.03-23.26 8-36.2 8s-25.13-2.97-36.2-8H208c-26.51 0-48 21.49-48 48v96c0 17.67 14.33 32 32 32z"></path></svg></span>
+                <span class="camera"><svg width="19" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"></path></svg></span>
+                <span class="info-circle"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg></span>
+                <span class="close"><svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path></svg></span>
+            </div>
         </div>
-        </div>
-        <?php if(get_option('plugin_l2d_jq')) echo '<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@2.1.4/dist/jquery.min.js"></script>'; ?>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/xb2016/kratos-pjax@0.3.6/static/js/live2d.js"></script>
+        <div class="waifu-btn" title="Waifu" style="display:none">看板娘</div>
 	</div><?php
 }
